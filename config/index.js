2b2a49b818733a98c06358debbabf79f491abd05
xiaoniezi <1839744180@qq.com>
2017-12-20 10:27:51 +0800

    server address
    uses localhost address

--- config/index.js
+++ config/index.js
@@ -13,9 +13,9 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    // host: 'localhost', // can be overwritten by process.env.HOST
-    host: '192.168.1.101', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: 'localhost', // can be overwritten by process.env.HOST
+    // host: '192.168.1.101', // can be overwritten by process.env.HOST
+    port: 8899, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
